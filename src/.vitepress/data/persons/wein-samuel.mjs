d14d2f720350070d7e2b552a76f8b9534e1de849
Samuel Wein <sam@samwein.com>
2024-11-25 14:11:25 +0100

    Update wein-samuel.mjs
    Filled out Information on self

--- src/.vitepress/data/persons/wein-samuel.mjs
+++ src/.vitepress/data/persons/wein-samuel.mjs
@@ -7,4 +7,28 @@ export default definePerson({
     team: TeamID.ABI,
     role: 'PostDoc',
     socialLinks: [],
+    email: 'samuel[dot]wein[at]uni-tuebingen.de',
+    socialLinks: [
+        { icon: 'github', link: 'https://github.com/poshul' },
+        { icon: 'mastodon', link: 'https://fediscience.org/@samweingamgee' },
+        { icon: 'linkedin', link: 'https://www.linkedin.com/in/samuel-wein-phd-20700323/' },
+        { icon: 'orcid', link: 'https://orcid.org/0000-0002-8923-6874/' },
+    ],
+    education: [
+        {
+            year: [2006, 2010],
+            value: 'Earlham College: BA (Biology/Computer Science) '
+        },
+        {
+            year: [2012, 2019],
+            value: 'University of Pennsylvania:  PhD (Biochemistry and Molecular Biophysics) '
+        }
+    ],
+    interests: [
+        'Open science',
+        'Open source software',
+        'Oligonucleotide mass spectrometry',
+        'Backpacking',
+        'Photography'
+    ]
 });
