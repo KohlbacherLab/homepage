e4da7a4e6e07660b8deeaa4be0d707c6b19bbaa0
Timo Sachsenberg <timo.sachsenberg@uni-tuebingen.de>
2024-11-25 12:42:46 +0100

    Update sachsenberg-timo.mjs

--- src/.vitepress/data/persons/sachsenberg-timo.mjs
+++ src/.vitepress/data/persons/sachsenberg-timo.mjs
@@ -2,8 +2,18 @@ import { TeamID } from "../..";
 import { definePerson } from "../..";
 
 export default definePerson({
+    address: [
+        "Room C322",
+        "Sand 14, Tübingen, Germany 72076"
+    ],
+    email: "timo.sachsenberg@uni-tuebingen.de",
+    phone: "+49-7071-29-70461",
     avatar: '/images/persons/sachsenberg-timo.png',
     name: 'Timo Sachsenberg',
     team: TeamID.ABI,
-    role: 'Postdoc',
+    role: 'Staff Scientist',
+    socialLinks: [
+        { icon: 'github', link: 'https://github.com/timosachsenberg' },
+        { icon: 'publications', link: 'https://scholar.google.com/citations?user=7H4EQacAAAAJ&hl=en' },
+    ],
 });
