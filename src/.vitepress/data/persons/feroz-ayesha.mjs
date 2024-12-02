465424611df9c030094ceb9987807755e2cb7365
AyeshaFeroz <102374028+AyeshaFerozAsad@users.noreply.github.com>
2024-12-02 15:19:57 +0100

    Added profile for Ayesha Feroz

--- src/.vitepress/data/persons/feroz-ayesha.mjs
+++ src/.vitepress/data/persons/feroz-ayesha.mjs
@@ -2,9 +2,13 @@ import { TeamID } from "../..";
 import { definePerson } from "../..";
 
 export default definePerson({
-    avatar: '/images/persons/default.png',
+    avatar:'/images/persons/feroz-ayesha.png',
     name: 'Ayesha Feroz',
     role: 'PhD Student',
     team: TeamID.ABI,
-    socialLinks: [],
+    socialLinks: [
+        { icon: 'github', link: 'https://github.com/Ayesha-Feroz' },
+        { icon: 'linkedin', link: 'https://pk.linkedin.com/in/ayesha-feroz-a426041a' },
+        { icon: 'twitter', link: 'https://x.com/AyeshaFeroz7?t=kfOOZ_3lzMtmHtOC8uCz' },
+    ],
 });
