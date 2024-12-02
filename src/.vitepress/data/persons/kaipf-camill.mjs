27531747d50a5b74f643dbacc4f1f8927e742871
ckaipf <camill@posteo.de>
2024-12-02 12:59:13 +0100

    Update data and image for profile

--- src/.vitepress/data/persons/kaipf-camill.mjs
+++ src/.vitepress/data/persons/kaipf-camill.mjs
@@ -5,5 +5,11 @@ export default definePerson({
     name: 'Camill Kaipf',
     team: TeamID.ABI,
     role: 'Researcher',
-    socialLinks: [],
+    email: 'camill.kaipf@uni-tuebingen.de',
+    phone: '+49 7071 2970482',
+    address: 'Sand 14, Room C121, Tübingen, 72076',
+    socialLinks: [
+        { icon: 'github', link: 'https://github.com/ckaipf' },
+        { icon: 'linkedin', link: 'www.linkedin.com/in/camill-kaipf-672532238' },
+    ],
 });
