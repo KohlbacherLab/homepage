564180fe197113a0999f11da3bc17ef6dd3bc211
KyowonJeong <jeong@informatik.uni-tuebingen.de>
2024-12-10 16:20:06 +0100

    Kyowon Jeong added

--- src/.vitepress/data/persons/jeong-kyowon.mjs
+++ src/.vitepress/data/persons/jeong-kyowon.mjs
@@ -3,7 +3,24 @@ import { TeamID, definePerson } from '../..';
 export default definePerson({
     avatar: '/images/persons/jeong-kyowon.png',
     name: 'Kyowon Jeong',
+    email: 'kyowon.jeong@uni-tuebingen.de',
     role: 'Postdoc',
     team: TeamID.ABI,
-    socialLinks: [],
+    socialLinks: [
+        { icon: 'github', link: 'https://github.com/kyowonjeong' },
+        { icon: 'twitter', link: 'https://twitter.com//eongKyowon' },
+        { icon: 'linkedin', link: 'https://www.linkedin.com/in/kyowon-jeong-35a7b3173/' },
+    ],
+    interests: [
+        'Top-down proteomics',
+        'FLASH * software',
+        'Proteoform resolved research'
+    ],
+    education: [
+        {
+            year: [2008, 2013],
+            value: 'UCSD'
+        },
+        // ...
+    ],
 });
