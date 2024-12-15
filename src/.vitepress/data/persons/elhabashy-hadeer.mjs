0b42a14c92899209c5e01e5f6783129f609c7d12
ElhabashyLab <167512520+ElhabashyLab@users.noreply.github.com>
2024-12-15 18:32:28 +0100

    Update elhabashy-hadeer.mjs

--- src/.vitepress/data/persons/elhabashy-hadeer.mjs
+++ src/.vitepress/data/persons/elhabashy-hadeer.mjs
@@ -1,9 +1,22 @@
 import { TeamID, definePerson } from '../..';
 
 export default definePerson({
+
+    email: 'hadeer.elhabashy@Tuebingen.mpg.de',
+    phone: '+49-7071-29-70457',
     avatar: '/images/persons/elhabashy-hadeer.png',
     name: 'Hadeer Elhabashy',
-    team: TeamID.ABI,
     role: 'Postdoc',
-    socialLinks: [],
+    team: TeamID.ABI,
+    socialLinks: [
+        { icon: 'github', link: 'https://github.com/ElhabashyLab' },
+        { icon: 'linkedin', link: 'linkedin.com/in/hadeer-elhabashy-88373254' }
+        { icon: 'linkedin', link: 'https://x.com/HadeerElhabashy' }
+    ],
+    education: [
+        {year: 2023, value: 'Ph.D. in Bioinformatics, University of Tübingen/Max Planck Institute for Biology Tübingen',},
+        {year: 2016, value: 'M.Sc. in Physics, The American University in Cairo',}
+        {year: 2012, value: 'preM.Sc. in Molecular BioPhysics, Cairo University',}
+        {year: 2010, value: 'B.Sc. in BioPhysics, Cairo University',}
+    ],
 });
