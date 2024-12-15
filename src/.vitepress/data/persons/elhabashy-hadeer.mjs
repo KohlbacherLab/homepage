6b4260cddf1313670cfa51c1b080998b2a2b63ac
ElhabashyLab <167512520+ElhabashyLab@users.noreply.github.com>
2024-12-15 18:05:03 +0100

    Update elhabashy-hadeer.mjs

--- src/.vitepress/data/persons/elhabashy-hadeer.mjs
+++ src/.vitepress/data/persons/elhabashy-hadeer.mjs
@@ -1,7 +1,7 @@
 import { TeamID, definePerson } from '../..';
 
 export default definePerson({
-    avatar: '/images/persons/elhabashi-hadeer.png',
+    avatar: '/images/persons/elhabashy-hadeer.png',
     name: 'Hadeer Elhabashy',
     team: TeamID.ABI,
     role: 'Postdoc',
