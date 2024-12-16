46cd0ee66d734718f3f53df10160a8980c6c18dc
ElhabashyLab <167512520+ElhabashyLab@users.noreply.github.com>
2024-12-16 10:46:19 +0100

    Update elhabashy-hadeer.mjs

--- src/.vitepress/data/persons/elhabashy-hadeer.mjs
+++ src/.vitepress/data/persons/elhabashy-hadeer.mjs
@@ -10,13 +10,13 @@ export default definePerson({
     team: TeamID.ABI,
     socialLinks: [
         { icon: 'github', link: 'https://github.com/ElhabashyLab' },
-        { icon: 'linkedin', link: 'linkedin.com/in/hadeer-elhabashy-88373254' },
-        { icon: 'x', link: 'https://x.com/HadeerElhabashy'},
+        { icon: 'linkedin', link: 'https://linkedin.com/in/hadeer-elhabashy-88373254' },
+        { icon: 'x', link: 'https://x.com/HadeerElhabashy' },
     ],
     education: [
-        {year: 2023, value: 'Ph.D. in Bioinformatics, University of Tübingen/Max Planck Institute for Biology Tübingen'},
-        {year: 2016, value: 'M.Sc. in Physics, The American University in Cairo'},
-        {year: 2012, value: 'preM.Sc. in Molecular BioPhysics, Cairo University'},
-        {year: 2010, value: 'B.Sc. in BioPhysics, Cairo University'},
+        { year: 2023, value: 'Ph.D. in Bioinformatics, University of Tübingen/Max Planck Institute for Biology Tübingen' },
+        { year: 2016, value: 'M.Sc. in Physics, The American University in Cairo' },
+        { year: 2012, value: 'preM.Sc. in Molecular BioPhysics, Cairo University' },
+        { year: 2010, value: 'B.Sc. in BioPhysics, Cairo University' },
     ],
 });
