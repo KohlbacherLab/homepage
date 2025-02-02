2ddc625c7fafbfdab40eebfd36f9b584edd7dfde
tada5hi <tada5hi@users.noreply.github.com>
2025-02-02 10:02:55 +0000

    deploy: 7273f3db6945b0c6269a4c99ac92e34cc9e589eb

---
+++ assets/index.md.DiXqKWGy.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,c as t,ai as i,o}from"./chunks/framework.BnfDpRyV.js";const b=JSON.parse('{"title":"KohlbacherLab","description":"","frontmatter":{},"headers":[],"relativePath":"index.md","filePath":"index.md"}'),n={name:"index.md"};function r(l,e,s,h,c,d){return o(),t("div",null,e[0]||(e[0]=[i("",8)]))}const m=a(n,[["render",r]]);export{b as __pageData,m as default};
