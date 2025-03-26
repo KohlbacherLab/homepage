820263d892ac86fd5e984876c93eedfbd36cb216
Travis Wrightsman <travis@wrightsman.org>
2025-03-26 15:30:51 +0100

    Add myself

---
+++ src/.vitepress/data/persons/wrightsman-travis.mjs
@@ -0,0 +1,17 @@
+import { definePerson } from '../..';
+
+export default definePerson({
+    name: 'Travis Wrightsman',
+    role: 'Bioinformatician',
+    avatar: 'https://github.com/twrightsman.png',
+    email: 'travis.wrightsman@uni-tuebingen.de',
+    phone: '+49(7071)29-70482',
+    address: 'Sand 14, Raum C121, 72076 Tübingen',
+    team: 'abi',
+    socialLinks: [
+        { icon: 'github', link: 'https://github.com/twrightsman' },
+    ],
+    interests: [
+        'Bioinformatics',
+    ],
+});
