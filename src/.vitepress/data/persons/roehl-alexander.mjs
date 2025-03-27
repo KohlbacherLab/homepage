e1dcb8a918872da25b796ccfbf2363a3d29e74f8
Nightknight3000 <alexander.roehl@uni-tuebingen.de>
2025-03-27 16:07:57 +0100

    fix: add missing commas

--- src/.vitepress/data/persons/roehl-alexander.mjs
+++ src/.vitepress/data/persons/roehl-alexander.mjs
@@ -1,5 +1,4 @@
-import { TeamID } from "../..";
-import { definePerson } from "../..";
+import { TeamID, definePerson } from '../..';
 
 export default definePerson({
     name: 'Alexander Röhl',
@@ -12,23 +11,23 @@ export default definePerson({
     socialLinks: [
         {
             icon: 'github',
-            link: 'https://github.com/Nightknight3000'
+            link: 'https://github.com/Nightknight3000',
         },
     ],
     interests: [
         'Data Privacy and Security',
         'Medical Data Analytics',
         'Machine Learning',
-        'Structural Bioinformatics'
+        'Structural Bioinformatics',
     ],
     education: [
         {
             year: 2020,
-            value: 'B.Sc. in Bioinformatics, University of Tübingen, Tübingen, Germany (Thesis: Subtypenspezifisches Machine Learning für die HIV-Korezeptor Vorhersage)'
+            value: 'B.Sc. in Bioinformatics, University of Tübingen, Tübingen, Germany (Thesis: Subtypenspezifisches Machine Learning für die HIV-Korezeptor Vorhersage)',
         },
         {
             year: 2022,
-            value: 'M.Sc. in Bioinformatics, University of Tübingen, Tübingen, Germany (Thesis: Method development in interaction prediction, structure elucidation, and modeling of protein-protein homo-oligomers)'
-        }
-    ]
+            value: 'M.Sc. in Bioinformatics, University of Tübingen, Tübingen, Germany (Thesis: Method development in interaction prediction, structure elucidation, and modeling of protein-protein homo-oligomers)',
+        },
+    ],
 });
