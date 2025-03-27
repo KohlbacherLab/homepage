1d4da8c1711d9c9f458127160885f0cba923e0a0
Nightknight3000 <alexander.roehl@uni-tuebingen.de>
2025-03-27 15:47:37 +0100

    feat: update info

--- src/.vitepress/data/persons/roehl-alexander.mjs
+++ src/.vitepress/data/persons/roehl-alexander.mjs
@@ -1,4 +1,5 @@
-import { TeamID, definePerson } from '../..';
+import { TeamID } from "../..";
+import { definePerson } from "../..";
 
 export default definePerson({
     name: 'Alexander Röhl',
@@ -11,25 +12,29 @@ export default definePerson({
     socialLinks: [
         {
             icon: 'github',
-            link: 'https://github.com/Nightknight3000',
+            link: 'https://github.com/Nightknight3000'
         },
     ],
     interests: [
         'Data Privacy and Security',
         'Medical Data Analytics',
         'Machine Learning',
-        'Structural Bioinformatics',
+        'Structural Bioinformatics'
     ],
     education: [
         {
-            year: 0,
-            value: 'tba',
+            year: 2020,
+            value: 'B.Sc. in Bioinformatics, University of Tübingen, Tübingen, Germany (Thesis: Subtypenspezifisches Machine Learning für die HIV-Korezeptor Vorhersage)'
         },
+        {
+            year: 2022,
+            value: 'M.Sc. in Bioinformatics, University of Tübingen, Tübingen, Germany (Thesis: Method development in interaction prediction, structure elucidation, and modeling of protein-protein homo-oligomers)'
+        }
     ],
-    publications: [
+    publications : [
         {
             year: 2024,
-            value: 'Röhl, A., Netz, E., Kohlbacher, O., & Elhabashy, H. (2024). CLAUDIO: automated structural analysis of cross-linking data. Bioinformatics, 40(4), btae146.',
-        },
-    ],
+            value: 'Röhl, A., Netz, E., Kohlbacher, O., & Elhabashy, H. (2024). CLAUDIO: automated structural analysis of cross-linking data. Bioinformatics, 40(4), btae146.'
+        }
+    ]
 });
