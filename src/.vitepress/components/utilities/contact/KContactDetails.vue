7a7ff057e9b653fd7e7f09848b29854afa5ef1a5
tada5hi <peter.placzek1996@gmail.com>
2026-04-21 12:04:22 +0200

    fix: rendering multiple phone numbers/addresses

--- src/.vitepress/components/utilities/contact/KContactDetails.vue
+++ src/.vitepress/components/utilities/contact/KContactDetails.vue
@@ -9,6 +9,13 @@ import type { PropType } from 'vue';
 import { computed, defineComponent } from 'vue';
 import type { ContactDetails } from './types.ts';
 
+function toArray(value?: string | string[]): string[] {
+    if (!value) {
+        return [];
+    }
+    return typeof value === 'string' ? [value] : value;
+}
+
 export default defineComponent({
     props: {
         entity: {
@@ -17,49 +24,91 @@ export default defineComponent({
         },
     },
     setup(props) {
-        const addresses = computed(() => {
-            if (!props.entity.address) {
-                return [];
-            }
-
-            if (typeof props.entity.address === 'string') {
-                return [props.entity.address];
-            }
-
-            return props.entity.address;
-        });
+        const phones = computed(() => toArray(props.entity.phone));
+        const faxes = computed(() => toArray(props.entity.fax));
+        const addresses = computed(() => toArray(props.entity.address));
 
         return {
+            phones,
+            faxes,
             addresses,
         };
     },
 });
 </script>
 <template>
-    <div class="d-flex flex-column">
+    <div class="d-flex flex-column contact-details gap-2">
         <div v-if="entity.email">
             <i class="fa fa-envelope pe-1" /> {{ entity.email }}
         </div>
-        <div v-if="entity.phone">
-            <i class="fa fa-phone pe-1" /> {{ entity.phone }}
+        <div v-if="phones.length === 1">
+            <i class="fa fa-phone pe-1" /> {{ phones[0] }}
         </div>
-        <div v-if="entity.fax">
-            <i class="fa fa-fax pe-1" /> {{ entity.fax }}
-        </div>
-        <div v-if="addresses && addresses.length > 0">
-            <div class="d-flex flex-row gap-1">
-                <div>
-                    <i class="fa fa-map-marker-alt pe-2" />
-                </div>
-                <div>
-                    <template
-                        v-for="(item, index) in addresses"
-                        :key="index"
-                    >
-                        {{ item }}<br>
-                    </template>
-                </div>
+        <div
+            v-else-if="phones.length > 1"
+            class="d-flex flex-row gap-1"
+        >
+            <div>
+                <i class="fa fa-phone pe-2" />
             </div>
+            <ul class="contact-details-list">
+                <li
+                    v-for="(item, index) in phones"
+                    :key="index"
+                >
+                    {{ item }}
+                </li>
+            </ul>
+        </div>
+        <div v-if="faxes.length === 1">
+            <i class="fa fa-fax pe-1" /> {{ faxes[0] }}
+        </div>
+        <div
+            v-else-if="faxes.length > 1"
+            class="d-flex flex-row gap-1"
+        >
+            <div>
+                <i class="fa fa-fax pe-2" />
+            </div>
+            <ul class="contact-details-list">
+                <li
+                    v-for="(item, index) in faxes"
+                    :key="index"
+                >
+                    {{ item }}
+                </li>
+            </ul>
+        </div>
+        <div v-if="addresses.length === 1">
+            <i class="fa fa-map-marker-alt pe-1" /> {{ addresses[0] }}
+        </div>
+        <div
+            v-else-if="addresses.length > 1"
+            class="d-flex flex-row gap-1"
+        >
+            <div>
+                <i class="fa fa-map-marker-alt pe-2" />
+            </div>
+            <ul class="contact-details-list">
+                <li
+                    v-for="(item, index) in addresses"
+                    :key="index"
+                >
+                    {{ item }}
+                </li>
+            </ul>
         </div>
     </div>
 </template>
+<style scoped>
+.contact-details-list {
+    list-style: disc;
+    padding-left: 1.25rem;
+    margin: 0;
+}
+
+.contact-details-list li {
+    margin: 0;
+    line-height: 1.4;
+}
+</style>
--- src/.vitepress/data/persons/nagel-andreas.mjs
+++ src/.vitepress/data/persons/nagel-andreas.mjs
@@ -4,8 +4,14 @@ export default definePerson({
     avatar: '/images/persons/nagel-andreas.png',
     name: 'Andreas Nagel',
     role: 'System Administrator',
-    phone: '+49 7071 29 70449',
-    address: 'Maria-von-Linden-Straße 6, Room 20-30/A21, 72076 Tübingen',
+    phone: [
+        '+49 7071 29 70456',
+        '+49 7071 29 70449',
+    ],
+    address: [
+        'Maria-von-Linden-Straße 6, Room 20-30/A21, 72076 Tübingen',
+        'Sand 14, Room C328, 7206 Tübingen'
+    ],
     team: TeamID.ABI,
     socialLinks: [],
 });
